adb7331374133ec37d7e1c8e2743d3072f2bad2b
israelr9897 <israelr9897@gmail.com>
2025-05-27 09:55:19 +0300

    Creation filds to commando

---
+++ Commando.cs
@@ -0,0 +1,12 @@
+namespace Commandos.Models
+{
+    public class Commandos
+    {
+        public string Name;
+        public string CodeName;
+        public string[] Tools;
+        public string Status;
+
+        
+    }
+}
