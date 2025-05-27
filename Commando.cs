64786960127cf2fa1db1959a76b443f4001076bd
israelr9897 <israelr9897@gmail.com>
2025-05-27 10:06:11 +0300

    Creation mathods to commando

--- Commando.cs
+++ Commando.cs
@@ -1,12 +1,37 @@
 namespace Commandos.Models
 {
-    public class Commandos
+    public class Commando
     {
         public string Name;
         public string CodeName;
         public string[] Tools;
         public string Status;
 
-        
+        public Commando(string name, string codeName, string[]tools, string status)
+        {
+            Name = name;
+            CodeName = codeName;
+            Tools = tools;
+            Status = status;
+       
+
+        }
+
+        public void Walk()
+        {
+            Status = "Walk";
+            System.Console.WriteLine("A soldier walks");
+        }
+        public void Hide()
+        {
+            Status = "Hide";
+            System.Console.WriteLine("A soldier is hiding.");
+        }
+        public void Attack()
+        {
+            Status = "Attack";
+            System.Console.WriteLine($"Commando with codename {CodeName} is active.");
+        }
+
     }
 }
